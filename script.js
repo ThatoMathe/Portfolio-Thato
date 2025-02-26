e6fa239930eb535211853b34d0263a0b3bb03781
Thato Mathe <131750290+ThatoMathe@users.noreply.github.com>
2025-02-26 15:12:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onlick = () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active')
 }
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        navbar.classList.remove('active');
+        menuIcon.classList.remove('bx-x');
+    }
+});
